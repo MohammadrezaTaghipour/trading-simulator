d4090766b37b20e85f8849129a4bc0dd3ded6808
m.taghipour <mohamadrezataghipour73@gmail.com>
2024-01-15 17:36:50 +0330

    add unit test: When_matching_sellOrder

--- test/TradingSimulator.Test.Domain/OrderBooks/When_matching_sellOrder.cs
+++ test/TradingSimulator.Test.Domain/OrderBooks/When_matching_sellOrder.cs
@@ -11,17 +11,28 @@ using Xunit;
 
 namespace TradingSimulator.Test.Domain.OrderBooks;
 
-public class When_matching_buyOrder
+public class When_matching_sellOrder
 {
     private readonly OrderBookTestSteps _ = new();
 
     [Fact]
-    public void BuyOrder_gets_matched_with_sellOrder_when_buyPrice_is_greaterThan_sellPrice()
+    public void SellOrder_gets_matched_with_buyOrder_when_sellPrice_is_lessThan_buyPrice()
     {
         var orderBookArguments = DefineOrderBookArgBuilder.Builder
             .With(a => a.Title, "order-book-A")
             .With(a => a.SessionId, SessionId.New())
             .With(a => a.SymbolId, SymbolId.New())
+            .Build();
+
+        var buyOrderArguments = PlaceOrderArgBuilder.Builder
+            .With(a => a.OrderId, OrderId.New())
+            .With(a => a.OrderBookId, orderBookArguments.Id)
+            .With(a => a.TraderId, TraderId.New())
+            .With(a => a.SessionId, orderBookArguments.SessionId)
+            .With(a => a.SymbolId, orderBookArguments.SymbolId)
+            .With(a => a.Cmd, OrderCommandType.Buy)
+            .With(a => a.Volume, new OrderVolume(100))
+            .With(a => a.Price, new Money(500))
             .Build();
 
         var sellOrderArguments = PlaceOrderArgBuilder.Builder
@@ -35,6 +46,28 @@ public class When_matching_buyOrder
             .With(a => a.Price, new Money(300))
             .Build();
 
+        var expected = new OrderMatchedEvent(orderBookArguments.Id,
+            buyOrderArguments.OrderId, sellOrderArguments.OrderId,
+            sellOrderArguments.Price, buyOrderArguments.Volume);
+
+        this
+            .Given(__ => _.TheCurrentDateTimeIs(DateTime.Now))
+            .And(__ => _.ThereIsADefinedOrderBookWithTheFollowingArguments(orderBookArguments))
+            .And(__ => _.ThereIsAPlacedOrderBookWithTheFollowingArguments(buyOrderArguments))
+            .When(__ => _.IPlaceAnOrderWithFollowingArguments(sellOrderArguments))
+            .Then(__ => _.ICanSeeThatMatchOrdersIsOccuredByRaisingTheFollowingEvent(expected))
+            .BDDfy();
+    }
+
+    [Fact]
+    public void SellOrder_also_gets_matched_with_buyOrder_when_sellPrice_is_equal_to_buyPrice()
+    {
+        var orderBookArguments = DefineOrderBookArgBuilder.Builder
+            .With(a => a.Title, "order-book-A")
+            .With(a => a.SessionId, SessionId.New())
+            .With(a => a.SymbolId, SymbolId.New())
+            .Build();
+
         var buyOrderArguments = PlaceOrderArgBuilder.Builder
             .With(a => a.OrderId, OrderId.New())
             .With(a => a.OrderBookId, orderBookArguments.Id)
@@ -44,28 +77,6 @@ public class When_matching_buyOrder
             .With(a => a.Cmd, OrderCommandType.Buy)
             .With(a => a.Volume, new OrderVolume(100))
             .With(a => a.Price, new Money(500))
-            .Build();
-
-        var expected = new OrderMatchedEvent(orderBookArguments.Id,
-            buyOrderArguments.OrderId, sellOrderArguments.OrderId,
-            buyOrderArguments.Price, buyOrderArguments.Volume);
-
-        this
-            .Given(__ => _.TheCurrentDateTimeIs(DateTime.Now))
-            .And(__ => _.ThereIsADefinedOrderBookWithTheFollowingArguments(orderBookArguments))
-            .And(__ => _.ThereIsAPlacedOrderBookWithTheFollowingArguments(sellOrderArguments))
-            .When(__ => _.IPlaceAnOrderWithFollowingArguments(buyOrderArguments))
-            .Then(__ => _.ICanSeeThatMatchOrdersIsOccuredByRaisingTheFollowingEvent(expected))
-            .BDDfy();
-    }
-
-    [Fact]
-    public void BuyOrder_also_gets_matched_with_sellOrder_when_buyPrice_is_equal_to_sellPrice()
-    {
-        var orderBookArguments = DefineOrderBookArgBuilder.Builder
-            .With(a => a.Title, "order-book-A")
-            .With(a => a.SessionId, SessionId.New())
-            .With(a => a.SymbolId, SymbolId.New())
             .Build();
 
         var sellOrderArguments = PlaceOrderArgBuilder.Builder
@@ -79,17 +90,6 @@ public class When_matching_buyOrder
             .With(a => a.Price, new Money(500))
             .Build();
 
-        var buyOrderArguments = PlaceOrderArgBuilder.Builder
-            .With(a => a.OrderId, OrderId.New())
-            .With(a => a.OrderBookId, orderBookArguments.Id)
-            .With(a => a.TraderId, TraderId.New())
-            .With(a => a.SessionId, orderBookArguments.SessionId)
-            .With(a => a.SymbolId, orderBookArguments.SymbolId)
-            .With(a => a.Cmd, OrderCommandType.Buy)
-            .With(a => a.Volume, new OrderVolume(100))
-            .With(a => a.Price, new Money(500))
-            .Build();
-
         var expected = new OrderMatchedEvent(orderBookArguments.Id,
             buyOrderArguments.OrderId, sellOrderArguments.OrderId,
             buyOrderArguments.Price, buyOrderArguments.Volume);
@@ -97,14 +97,14 @@ public class When_matching_buyOrder
         this
             .Given(__ => _.TheCurrentDateTimeIs(DateTime.Now))
             .And(__ => _.ThereIsADefinedOrderBookWithTheFollowingArguments(orderBookArguments))
-            .And(__ => _.ThereIsAPlacedOrderBookWithTheFollowingArguments(sellOrderArguments))
-            .When(__ => _.IPlaceAnOrderWithFollowingArguments(buyOrderArguments))
+            .And(__ => _.ThereIsAPlacedOrderBookWithTheFollowingArguments(buyOrderArguments))
+            .When(__ => _.IPlaceAnOrderWithFollowingArguments(sellOrderArguments))
             .Then(__ => _.ICanSeeThatMatchOrdersIsOccuredByRaisingTheFollowingEvent(expected))
             .BDDfy();
     }
 
     [Fact]
-    public void BuyOrder_gets_matched_with_multiple_sellOrders_when_it_has_greater_volume_than_sellOrder_volume()
+    public void SellOrder_gets_matched_with_multiple_buyOrders_when_it_has_greater_volume_than_buyOrder_volume()
     {
         var orderBookArguments = DefineOrderBookArgBuilder.Builder
             .With(a => a.Title, "order-book-A")
@@ -112,55 +112,55 @@ public class When_matching_buyOrder
             .With(a => a.SymbolId, SymbolId.New())
             .Build();
 
-        var sellOrderArguments1 = PlaceOrderArgBuilder.Builder
-            .With(a => a.OrderId, OrderId.New())
-            .With(a => a.OrderBookId, orderBookArguments.Id)
-            .With(a => a.TraderId, TraderId.New())
-            .With(a => a.SessionId, orderBookArguments.SessionId)
-            .With(a => a.SymbolId, orderBookArguments.SymbolId)
-            .With(a => a.Cmd, OrderCommandType.Sell)
-            .With(a => a.Volume, new OrderVolume(100))
-            .With(a => a.Price, new Money(400))
-            .Build();
-
-        var sellOrderArguments2 = PlaceOrderArgBuilder.Builder
-            .With(a => a.OrderId, OrderId.New())
-            .With(a => a.OrderBookId, orderBookArguments.Id)
-            .With(a => a.TraderId, TraderId.New())
-            .With(a => a.SessionId, orderBookArguments.SessionId)
-            .With(a => a.SymbolId, orderBookArguments.SymbolId)
-            .With(a => a.Cmd, OrderCommandType.Sell)
-            .With(a => a.Volume, new OrderVolume(100))
-            .With(a => a.Price, new Money(400))
-            .Build();
-
-        var buyOrderArguments = PlaceOrderArgBuilder.Builder
+        var buyOrderArguments1 = PlaceOrderArgBuilder.Builder
             .With(a => a.OrderId, OrderId.New())
             .With(a => a.OrderBookId, orderBookArguments.Id)
             .With(a => a.TraderId, TraderId.New())
             .With(a => a.SessionId, orderBookArguments.SessionId)
             .With(a => a.SymbolId, orderBookArguments.SymbolId)
             .With(a => a.Cmd, OrderCommandType.Buy)
+            .With(a => a.Volume, new OrderVolume(100))
+            .With(a => a.Price, new Money(400))
+            .Build();
+
+        var buyOrderArguments2 = PlaceOrderArgBuilder.Builder
+            .With(a => a.OrderId, OrderId.New())
+            .With(a => a.OrderBookId, orderBookArguments.Id)
+            .With(a => a.TraderId, TraderId.New())
+            .With(a => a.SessionId, orderBookArguments.SessionId)
+            .With(a => a.SymbolId, orderBookArguments.SymbolId)
+            .With(a => a.Cmd, OrderCommandType.Buy)
+            .With(a => a.Volume, new OrderVolume(100))
+            .With(a => a.Price, new Money(400))
+            .Build();
+
+        var sellOrderArguments = PlaceOrderArgBuilder.Builder
+            .With(a => a.OrderId, OrderId.New())
+            .With(a => a.OrderBookId, orderBookArguments.Id)
+            .With(a => a.TraderId, TraderId.New())
+            .With(a => a.SessionId, orderBookArguments.SessionId)
+            .With(a => a.SymbolId, orderBookArguments.SymbolId)
+            .With(a => a.Cmd, OrderCommandType.Sell)
             .With(a => a.Volume, new OrderVolume(200))
-            .With(a => a.Price, new Money(500))
+            .With(a => a.Price, new Money(300))
             .Build();
 
         var expected = new[]
         {
             new OrderMatchedEvent(orderBookArguments.Id,
-                buyOrderArguments.OrderId, sellOrderArguments1.OrderId,
-                buyOrderArguments.Price, buyOrderArguments.Volume - sellOrderArguments1.Volume),
+                buyOrderArguments1.OrderId, sellOrderArguments.OrderId,
+                sellOrderArguments.Price, sellOrderArguments.Volume - buyOrderArguments1.Volume),
             new OrderMatchedEvent(orderBookArguments.Id,
-                buyOrderArguments.OrderId, sellOrderArguments2.OrderId,
-                buyOrderArguments.Price, sellOrderArguments2.Volume),
+                buyOrderArguments2.OrderId, sellOrderArguments.OrderId,
+                sellOrderArguments.Price, buyOrderArguments2.Volume),
         };
 
         this
             .Given(__ => _.TheCurrentDateTimeIs(DateTime.Now))
             .And(__ => _.ThereIsADefinedOrderBookWithTheFollowingArguments(orderBookArguments))
-            .And(__ => _.ThereIsAPlacedOrderBookWithTheFollowingArguments(sellOrderArguments1))
-            .And(__ => _.ThereIsAPlacedOrderBookWithTheFollowingArguments(sellOrderArguments2))
-            .When(__ => _.IPlaceAnOrderWithFollowingArguments(buyOrderArguments))
+            .And(__ => _.ThereIsAPlacedOrderBookWithTheFollowingArguments(buyOrderArguments1))
+            .And(__ => _.ThereIsAPlacedOrderBookWithTheFollowingArguments(buyOrderArguments2))
+            .When(__ => _.IPlaceAnOrderWithFollowingArguments(sellOrderArguments))
             .Then(__ => _.ICanSeeThatMatchOrdersIsOccuredByRaisingTheFollowingEvents(expected))
             .BDDfy();
     }
